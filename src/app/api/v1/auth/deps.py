cf02ddd08bcc09dc4887d4155fe089d10d0d0cc0
OliverW <1225191678@qq.com>
2026-01-02 13:29:27 +0800

    认证权限公共方法调整

--- src/app/api/v1/auth/deps.py
+++ src/app/api/v1/auth/deps.py
@@ -1,12 +1,7 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session
 
 from app.core.db import get_db_session
-from app.core.exceptions import NotFoundError
-from app.core.jwt import decode_token
-from app.core.security import oauth2_scheme
-from app.models.iam import User
 from app.services.auth import LoginService
 from app.services.auth.registration import RegistrationService
 
@@ -17,42 +12,3 @@ def get_registration_service(db: AsyncSession = Depends(get_db_session)) -> Regi
 
 def get_login_service(db: AsyncSession = Depends(get_db_session)) -> LoginService:
     return LoginService(db)
-
-
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db_session)):
-    """
-    验证token有效性，并获取用户对象
-    """
-    try:
-        payload = decode_token(token)
-        username = payload.get("sub")
-        user = db.query(User).filter(User.username == username).first()
-        if not user:
-            raise NotFoundError(f"User '{username}' not found")
-        return user
-    except Exception as e:
-        # 其它解码异常可以用自定义异常
-        raise NotFoundError("Invalid token") from e
-
-
-def has_role(role_name: str):
-    def dependency(user: User = Depends(get_current_user)):
-        if user.is_superuser:  # 超级管理员默认通过
-            return user
-        if role_name not in [r.name for r in user.roles]:
-            raise NotFoundError("Insufficient role")
-        return user
-
-    return dependency
-
-
-def has_perm(permission_name: str):
-    def dependency(user: User = Depends(get_current_user)):
-        if user.is_superuser:
-            return user
-        permissions = {p.name for r in user.roles for p in r.permissions}
-        if permission_name not in permissions:
-            raise NotFoundError("Insufficient permission")
-        return user
-
-    return dependency
--- src/app/core/security.py
+++ src/app/core/security.py
@@ -1,10 +1,15 @@
 from fastapi import Request, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
 
+from app.core.db import get_db_session
+from app.core.exceptions import NotFoundError
 from app.core.exceptions import UnauthorizedError
+from app.core.jwt import decode_token
 from app.core.jwt import verify_token
 from app.core.settings import settings
+from app.models.iam import User
 
 
 class ServiceOAuth2PasswordBearer(OAuth2PasswordBearer):
@@ -35,3 +40,42 @@ async def login_required(token: str = Depends(oauth2_scheme)) -> dict:
     if not payload or payload.get("sub") is None:
         raise UnauthorizedError("Token payload missing subject")
     return payload
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db_session)):
+    """
+    验证token有效性，并获取用户对象
+    """
+    try:
+        payload = decode_token(token)
+        username = payload.get("sub")
+        user = db.query(User).filter(User.username == username).first()
+        if not user:
+            raise NotFoundError(f"User '{username}' not found")
+        return user
+    except Exception as e:
+        # 其它解码异常可以用自定义异常
+        raise NotFoundError("Invalid token") from e
+
+
+def has_role(role_name: str):
+    def dependency(user: User = Depends(get_current_user)):
+        if user.is_superuser:  # 超级管理员默认通过
+            return user
+        if role_name not in [r.name for r in user.roles]:
+            raise NotFoundError("Insufficient role")
+        return user
+
+    return dependency
+
+
+def has_perm(permission_name: str):
+    def dependency(user: User = Depends(get_current_user)):
+        if user.is_superuser:
+            return user
+        permissions = {p.name for r in user.roles for p in r.permissions}
+        if permission_name not in permissions:
+            raise NotFoundError("Insufficient permission")
+        return user
+
+    return dependency
